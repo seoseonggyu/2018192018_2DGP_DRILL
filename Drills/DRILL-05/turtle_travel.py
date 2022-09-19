d4f2f7c70e5813cf852171ff4f52178ab6252dab
seoseonggyu <112987897+seoseonggyu@users.noreply.github.com>
2022-09-19 21:00:08 +0900

    drill 05 완성
    2018192018 서성규

---
+++ Drills/DRILL-05/turtle_travel.py
@@ -0,0 +1,34 @@
+import turtle
+
+def move_up():
+    turtle.stamp()
+    turtle.setheading(90)
+    turtle.forward(50)
+
+def move_down():
+    turtle.stamp()
+    turtle.setheading(-90)
+    turtle.forward(50)
+
+def move_left():
+    turtle.stamp()
+    turtle.setheading(180)
+    turtle.forward(50)
+
+def move_right():
+    turtle.stamp()
+    turtle.setheading(0)
+    turtle.forward(50)
+
+def restart():
+    turtle.reset()
+
+turtle.shape("turtle")
+
+turtle.onkey(move_right,'d')
+turtle.onkey(move_left,'a')
+turtle.onkey(move_up,'w')
+turtle.onkey(move_down,'s')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
+
