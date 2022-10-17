b97e6d753bbb7c780d183f36e1e5a75c2ab613b4
seoseonggyu <112987897+seoseonggyu@users.noreply.github.com>
2022-10-17 13:16:02 +0900

    drill 10 완성
    2018192018 서성규

---
+++ Drills/DRILL-10/main.py
@@ -0,0 +1,17 @@
+import pico2d
+import play_state
+import logo_state
+
+
+pico2d.open_canvas()
+
+states = [logo_state, play_state]
+for state in states:
+    state.enter()
+    while state.running:
+        state.handle_events()
+        state.update()
+        state.draw()
+    state.exit()
+
+pico2d.close_canvas()
